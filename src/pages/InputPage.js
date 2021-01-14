9edd7958abae721e96ec25e08ec36c7d2978430e
henryhansollee <henryhansollee@gmail.com>
2021-01-14 19:12:32 +0900

    Create pages

---
+++ src/pages/InputPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './InputPage.scss';
+
+const InputPage = () => {
+  return (
+    <div>
+      <h1>InputPage</h1>
+    </div>
+  );
+};
+
+export default InputPage;
---
+++ src/pages/ResultPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ResultPage.scss';
+
+const ResultPage = () => {
+  return (
+    <div>
+      <h1>ResultPage</h1>
+    </div>
+  );
+};
+
+export default ResultPage;
