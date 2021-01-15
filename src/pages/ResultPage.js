fa7c864cc80cf55d1bab2381486a730006dc3a1f
henryhansollee <henryhansollee@gmail.com>
2021-01-15 15:27:56 +0900

    Get params data

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ResultPage.scss';
 
-const ResultPage = () => {
+const ResultPage = ({match}) => {
   return (
     <div>
       <h1>ResultPage</h1>
+      <h2>{match.params.teamNums}</h2>
       <Link to="/">처음으로</Link>
     </div>
   );
