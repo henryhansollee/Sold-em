73cc2596163c2359002b0b6145c5a01856f0e53f
henryhansollee <henryhansollee@gmail.com>
2021-01-15 16:11:27 +0900

    Create standard nums

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './ResultPage.scss';
 
-const ResultPage = ({match}) => {
+const ResultPage = ({ match }) => {
+  const [ standardNums, setStandardNums ] = useState('');
+  const standard = [];
+  useEffect(() => {
+    for (var i = 1; i <= match.params.teamNums; i++) {
+      standard.push(i)
+    }
+    setStandardNums(standard)
+  }, [])
+
   return (
     <div>
       <h1>ResultPage</h1>
       <h2>{match.params.teamNums}</h2>
+      <h2>기준: {standardNums}</h2>
       <Link to="/">처음으로</Link>
     </div>
   );
