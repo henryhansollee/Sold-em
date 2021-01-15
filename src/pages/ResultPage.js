921a3cbbfee0f8026fcc63ce5dcacf71e77e7e38
henryhansollee <henryhansollee@gmail.com>
2021-01-15 16:18:47 +0900

    Create suffle func

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -4,6 +4,7 @@ import './ResultPage.scss';
 
 const ResultPage = ({ match }) => {
   const [ standardNums, setStandardNums ] = useState('');
+  const [ suffledNums, setSuffledNums ] = useState('');
   const standard = [];
   useEffect(() => {
     for (var i = 1; i <= match.params.teamNums; i++) {
@@ -12,11 +13,21 @@ const ResultPage = ({ match }) => {
     setStandardNums(standard)
   }, [])
 
+  const suffling = () => {
+    for (var i = 0; i < standardNums.length; i++) {
+      var j = Math.floor(Math.random() * (i + 1));
+      [standardNums[i], standardNums[j]] = [standardNums[j], standardNums[i]];
+      setSuffledNums(standardNums)
+    }
+  }
+
   return (
     <div>
       <h1>ResultPage</h1>
       <h2>{match.params.teamNums}</h2>
       <h2>기준: {standardNums}</h2>
+      <h3>셔플: {suffledNums}</h3>
+      <button onClick={suffling}>셔플</button>
       <Link to="/">처음으로</Link>
     </div>
   );
