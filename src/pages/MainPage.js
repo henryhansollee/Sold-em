c2392b5f17e46e6a69482885433c0ba83d85b760
henryhansollee <henryhansollee@gmail.com>
2021-01-19 19:46:22 +0900

    Add modal

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // ROUTER
 import { Link } from 'react-router-dom';
@@ -13,10 +13,17 @@ import main3 from '../assets/images/main3.png'
 import main4 from '../assets/images/main4.png'
 import main5 from '../assets/images/main5.png'
 
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
 // CSS
 import './MainPage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MainPage = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <ReactFullpage
       licenseKey = {'YOUR_KEY_HERE'}
@@ -33,6 +40,25 @@ const MainPage = () => {
                 <img src={main4} alt="main4" className="main-img" />
                 <img src={main3} alt="main3" className="main-img" />
               </div>
+
+              <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+              </Button>
+
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
               <Link to="/input">시작하기</Link>
             </div>
           </ReactFullpage.Wrapper>
