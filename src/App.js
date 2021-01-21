89c477dd866d62f52221f89cdc76ccffc14ab83d
henryhansollee <henryhansollee@gmail.com>
2021-01-21 20:11:07 +0900

    Add title and favicon

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import ResultPage from './pages/ResultPage';
+import { Helmet } from 'react-helmet-async';
 
 const App = () => {
   return (
     <div>
+      <Helmet>
+        <title>싸피솔덤</title>
+      </Helmet>
       <Route component={MainPage} exact path="/" />
       <Route component={ResultPage} exact path="/result/:teamNums" />
     </div>
